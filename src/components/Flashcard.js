54375e0316b68d2f018df33bdf204ba78cf8224d
tessaSAC <tessaSAC@users.noreply.github.com>
2017-01-06 15:41:49 -0500

    adds logo to top

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -43,6 +43,7 @@ class Flashcard extends Component {
 		return (
 			<div>
 				<div className="row center">
+					<img src="/media/logo/logo-text_only.png"
 					<br />
 					<br />
 					<div className="col s2">
