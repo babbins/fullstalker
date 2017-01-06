8f64b185166fc5a09a5929a456096571315e948e
Sia <schen22@gmail.com>
2017-01-06 15:12:40 -0500

    style

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
 export default (props) => (
-	<div>
-		{
-			!props.flipped
-				? (<button className="btn" onClick={props.flipCard}>FLIP</button>)
-				: (
-					<div>
-						<button className="btn">X</button> <button className="btn">O</button>
-					</div>
-				  )
-		}
+	<div className="center">
+
+
+		<div className="row">
+			<div className="col s3"></div>
+			<div className="col s6">
+				{
+					!props.flipped
+						? (<button className="btn amber darken-3" onClick={props.flipCard}>FLIP</button>)
+						: (
+							<div>
+								<button className="btn amber darken-3">X</button> <button className="btn amber darken-3">O</button>
+							</div>
+						  )
+				}
+			</div>
+			<div className="col s3"></div>
+		</div>
+
 	</div>
 );
